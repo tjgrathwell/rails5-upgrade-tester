5b50f03ef23d7b0c4b2f3a45017130148ff31a38
Travis Grathwell <tjgrathwell@gmail.com>
2016-10-10 16:06:07 -0700

    Rails 5 upgrade, fixing everything but params deprecations

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 ruby '2.3.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.4'
+gem 'rails'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_rails5-upgrade-tester_session'
+Rails.application.config.session_store :cookie_store, key: '_rails5_upgrade_tester_session'
